49d6e8802894933e95af42d7b77464cfb0443847
louiscklaw <louiscklaw@gmail.com>
2019-09-30 14:14:00 +0800

    update,

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,36 +1,14 @@
-const outerFrame = `
-<section class="section">
-<div class="container">
-    <div class="columns">
-        $innerFrame$
-    </div>
-</div>
-</section>
-`;
-let innerFrame = `<div class="column">$frame_content$</div>`;
-
-function query_google_trend(geo_in) {
-  return `<script> trends.embed.renderWidget( "dailytrends", "", {
-        "geo": geo_in,
-        "guestPath": "https://trends.google.com:443/trends/embed/"
-    } )</script>`;
-}
-
-function create_inner_frame(frame_content) {
-  return innerFrame.replace('$frame_content$', frame_content);
-}
-
-function create_outer_frame(frame_content) {
-  return outerFrame.replace('$innerFrame$', frame_content);
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-  console.log("helloworld"); // document.querySelector( '.app' ).innerHTML = create_outer_frame(
-  //     create_inner_frame(query_google_trend('HK'))
-  // );
-  // document.querySelector( '.app' ).innerHTML = create_outer_frame(create_inner_frame(query_google_trend('HK')));
+  console.log("helloworld");
+  let test = document.getElementById("trends-widget-1");
+  let column_list = document.querySelectorAll('.column');
+  column_list.forEach(column => {
+    console.log('findme');
+    column.addEventListener('mouseenter', function (event) {
+      column.classList.add('is-half');
+    });
+    column.addEventListener('mouseleave', function (event) {
+      column.classList.remove('is-half');
+    });
+  });
 });
-
-function helloworld(text_in) {
-  console.log(text_in);
-}
